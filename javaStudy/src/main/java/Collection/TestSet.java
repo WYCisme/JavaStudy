637d2f6316bb2632364541602a18627bf8fd15a2
WYC_lenovo <1432649111@qq.com>
2020-08-26 12:56:37 +0800

    ,,,

--- javaStudy/src/main/java/Collection/TestSet.java
+++ javaStudy/src/main/java/Collection/TestSet.java
@@ -1,8 +1,12 @@
 package Collection;
 
+import org.junit.Test;
+
 import java.util.HashSet;
 import java.util.Set;
 import java.util.TreeMap;
+
+import static cn.hutool.core.lang.Console.log;
 
 /**
  * @projectName: javaStudy
@@ -12,12 +16,17 @@ import java.util.TreeMap;
  * @description: 连我自己都不知道在写什么......
  */
 public class TestSet {
-    public static void main(String[] args) {
+
+    @Test
+    public void test() {
 
         TreeMap<String,Integer> treeMap =new TreeMap<>();
         treeMap.put("A",666);
 
         Set<String> set=new HashSet<>();
+
+        set.add(treeMap.toString());
+        log("set:{}",set);
     }
 
 }
